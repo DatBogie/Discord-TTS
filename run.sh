45c3d29ffa07ecb17b7fd3e1b79979174396ce12
Dat Bogie <masterbuildermm2009@gmail.com>
2026-02-15 16:47:48 -0500

    Initial commit

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # shellcheck disable=SC1091
 . ./.venv/bin/activate
-pip3 install -r requirements.txt
 python3 main.py
--- setup.bat
+++ setup.bat
@@ -1,4 +1,5 @@
 @echo off
 python -m venv .venv
 .\.venv\Scripts\activate.bat
+pip3 install -r requirements.txt
 python -m piper.download_voices en_US-amy-medium
